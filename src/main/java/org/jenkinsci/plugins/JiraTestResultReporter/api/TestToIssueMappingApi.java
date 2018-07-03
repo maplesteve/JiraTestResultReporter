6cb3455ae468008615bf11ebebe7865ac4d909f6
Desai <kd48432@bose.com>
2018-07-03 17:55:26 +0530

    JENKINS-52341 - added logic to handle project residing under sub-folder

--- src/main/java/org/jenkinsci/plugins/JiraTestResultReporter/api/TestToIssueMappingApi.java
+++ src/main/java/org/jenkinsci/plugins/JiraTestResultReporter/api/TestToIssueMappingApi.java
@@ -6,6 +6,7 @@ import com.google.gson.JsonElement;
 import hudson.matrix.MatrixProject;
 import hudson.model.Api;
 import hudson.model.Job;
+import hudson.model.Item;
 import jenkins.model.Jenkins;
 import org.jenkinsci.plugins.JiraTestResultReporter.TestToIssueMapping;
 import org.kohsuke.stapler.StaplerRequest;
@@ -42,16 +43,30 @@ public class TestToIssueMappingApi extends Api {
             return;
         }
 
-        //sub job of a matrix project
+        // sub job of a matrix project or a folder
         if(jobName.contains("/")) {
             String matrixJobName = jobName.split("/")[0];
             String matrixSubJobName = jobName.split("/")[1];
-            MatrixProject matrixProject = (MatrixProject) Jenkins.getActiveInstance().getItem(matrixJobName);
-            if(matrixProject == null) {
-                rsp.sendError(HttpServletResponse.SC_NOT_FOUND);
-                return;
+            Item item = Jenkins.getActiveInstance().getItem(matrixJobName);
+
+            // check if it is matrix project
+            if(item.getClass().equals(MatrixProject.class)) {
+                MatrixProject matrixProject = (MatrixProject) item;
+                if(matrixProject == null) {
+                    rsp.sendError(HttpServletResponse.SC_NOT_FOUND);
+                    return;
+                }
+                result = TestToIssueMapping.getInstance().getMap(matrixProject, matrixSubJobName);
             }
-            result = TestToIssueMapping.getInstance().getMap(matrixProject, matrixSubJobName);
+            // else consider job resides in a sub-folder
+            else {
+                Job job = (Job) Jenkins.getActiveInstance().getItemByFullName(jobName);
+                if (job == null) {
+                    rsp.sendError(HttpServletResponse.SC_NOT_FOUND);
+                    return;
+                }
+                result = TestToIssueMapping.getInstance().getMap(job);
+            }
         // top level job (either matrix, freestyle or maven
         } else {
             Job job = (Job) Jenkins.getActiveInstance().getItem(jobName);
